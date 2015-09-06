4977fba116cc9292b47c956747d621a402b53a93
Jordi Serrano Berbel <jsberbel95@gmail.com>
2015-09-06 20:39:32 +0200

    No miris l'escena de la biblio

--- Assets/Scripts/bibliotecaManager.js
+++ Assets/Scripts/bibliotecaManager.js
@@ -1,7 +1,8 @@
 ﻿#pragma strict
 
+private var endVideo:boolean = false;
 private var alpha:float = 1.0f;
-private var fadeTitle:boolean = false;
+static var restart:boolean = false;
 
 private var mainCameraScript:bibliotecaCameraBehaviour;
 //private var mainCameraMovie:MovieTexture;
@@ -10,16 +11,26 @@ var mainCamera:GameObject;
 var leftDoor:GameObject;
 var rightDoor:GameObject;
 
-var alphaImg:Texture2D;
+var blackImage:Texture2D;
 var atrezzo:Transform;
+
+//------fade transition---------
+/*var fadeImage:Texture2D;
+private var xFrame:double = 0.8;
+private var yFrame:double = 0.0;
+private var timer:double = 0.0;
+private var frameWait:double = 0.02;
+private var curCol:int = 0;*/
 
 function OnGUI() {
 	GUI.color.a = alpha;
-	//GUI.DrawTexture(new Rect(0,0,Screen.width, Screen.height), mainCameraMovie, ScaleMode.ScaleAndCrop, true, 0.0f);
-	
+	GUI.DrawTexture(new Rect(0,0,Screen.width, Screen.height), blackImage);
+	//GUI.DrawTextureWithTexCoords(new Rect(0,0,Screen.width, Screen.height), fadeImage, new Rect(xFrame,yFrame,0.2, 0.2), true);
 }
+
 function Awake() {
-	Handheld.PlayFullScreenMovie ("IntroPageFlip3.mp4", Color.black, FullScreenMovieControlMode.CancelOnInput);
+	if (!restart) PlayIntro("IntroPageFlip4.mp4");
+	else endVideo = true;
 }
 
 function Start() {
@@ -27,22 +38,30 @@ function Start() {
 	//mainCameraMovie = mainCamera.GetComponent(Renderer).material.mainTexture as MovieTexture;
 	leftDoor.GetComponent(Animation).enabled = false;
 	rightDoor.GetComponent(Animation).enabled = false;
-	yield WaitForSeconds(10.0);
-	PlayGame();
+}
+
+function PlayIntro(videoPath:String) {
+ 	Handheld.PlayFullScreenMovie(videoPath, Color.black, FullScreenMovieControlMode.CancelOnInput);  
+ 	yield WaitForEndOfFrame();
+ 	alpha = 1.0f;
+ 	Debug.Log("Video playback completed.");
+ 	endVideo = true;
+ 	restart = true;
 }
 
 function Update() {
-
-	if (fadeTitle) alpha -= Time.deltaTime;
 	
-	if (Input.touchCount > 0 || Input.GetMouseButtonDown(0)){
-		//PlayGame();
+	if (alpha <= 0.0f && endVideo)  {
+		endVideo = false;
+		PlayGame();
 	}
+	else if (endVideo) alpha -= Time.deltaTime*0.4f;
+	
+	//if (Input.touchCount > 0 || Input.GetMouseButtonDown(0)){}
 }
 
 function PlayGame() {
-	fadeTitle = true;
-	yield WaitForSeconds(1.0);
+	yield WaitForSeconds(0.5);
 	
 	leftDoor.GetComponent(Animation).enabled = true;
 	leftDoor.GetComponent(Animation).Play("OpenLeftDoor");
@@ -54,116 +73,19 @@ function PlayGame() {
 	for (var child:Transform in atrezzo) Destroy(child.gameObject);
 	Destroy(leftDoor);
 	Destroy(rightDoor);
-	Application.LoadLevel ("Main");
+	Application.LoadLevel (1);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*private var xWords:double[];
-private var yWords:double[];
-private var nWords:int = 5;
-
-private var sxPlay:double = 100;
-private var syPlay:double = 100;
-private var sdPlay:int = 1;
-
-//---------textures---------
-var background:Texture;
-var movingWords:Texture; //video
-var play:Texture;
-
-//---------GUIstyles---------
-var titleGuiStyle:GUIStyle;
-
-function Start () {
-	xWords = new double[nWords];
-	yWords = new double[nWords];
-}
-
-function OnGUI() {
-    if(!movingWords) Debug.LogError("Assign a Texture in the inspector.");
-	else {
-		 for(var i : int = 0; i< nWords; i++) {
-			GUI.DrawTexture(Rect(xWords[i],yWords[i],Screen.width,Screen.height), movingWords, ScaleMode.StretchToFill, true, 0.0f);
-		}
-	}
-    
-	if(!background) Debug.LogError("Assign a Texture in the inspector.");
-	else GUI.DrawTexture(Rect(0,0,Screen.width,Screen.height), background, ScaleMode.StretchToFill, true, 0.0f);
-	
-	if(!titleGuiStyle) Debug.LogError("Assign a GUIStyle in the inspector.");
-    else GUI.Label (new Rect (Screen.width/2, Screen.height/3, 5, 5), "SERRAPLO_CATA15", titleGuiStyle);
-    
-    if(!play) Debug.LogError("Assign a Texture in the inspector.");
-	else GUI.DrawTexture(Rect(Screen.width/2-sxPlay/2,Screen.height/1.6-syPlay/2,sxPlay,syPlay), play, ScaleMode.ScaleToFit, true, 0.0f);
-	
-	if (Input.touchCount > 0 || Input.GetMouseButtonDown(0)){
-		Application.LoadLevel ("Main");
-	}
-}
-
-function Update () {
-	
-	for (var i : int = 0; i< nWords; i++) {
-		if (xWords[i] < -800) xWords[i] = 800;
-		if (yWords[i] > 800) yWords[i] = -800;
-		xWords[i] -= Time.deltaTime*i*20.0;
-		yWords[i] += Time.deltaTime*i*20.0;
-	}
-	
-	if (sxPlay > 120) sdPlay = -1;
-	else if (sxPlay < 100) sdPlay = 1;
-	
-	sxPlay += Time.deltaTime*30.0*sdPlay;
-	syPlay += Time.deltaTime*30.0*sdPlay;
-	
-
+/*function FadeOut() {
+	GUI.DrawTextureWithTexCoords(new Rect(0,0,Screen.width, Screen.height), fadeImage, new Rect(xFrame,yFrame,0.2, 0.2), true);
+	if(Time.time - timer >= frameWait) {
+        if (xFrame <= 0.0) {
+       		xFrame = 0.8;
+       		if (curCol > 3) fadingIn = false;
+       		else curCol++;
+			yFrame = 0.2*curCol;
+        }
+		else xFrame -= 0.2;
+		timer = Time.time;
+    }
 }*/
