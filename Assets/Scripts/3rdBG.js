e91f5fc3d1a3746009cafc8da35d750bc38b67a5
thePLO <pdemiguel@enti.cat>
2015-09-19 18:05:37 +0200

    terres arreglats + inici BGs

---
+++ Assets/Scripts/3rdBG.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+private var bg1:Transform;	
+private var bg2:Transform;
+var player:GameObject;
+
+function Start () {
+	bg1 = transform.FindChild("p1");
+	bg2 = transform.FindChild("p2");
+}
+
+function Update () {
+	if (player.transform.position.x>bg1.position.x+30){
+		bg1.position.x+=60;
+	}
+	if (player.transform.position.x>bg2.position.x+30){
+		bg2.position.x+=60;
+	}
+}
