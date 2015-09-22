8268679361e32db1bb6d533a1b412b8322b2c816
thePLO <pdemiguel@enti.cat>
2015-09-22 19:23:28 +0200

    inici tenda

--- Assets/Scripts/bibliotecaManager.js
+++ Assets/Scripts/bibliotecaManager.js
@@ -5,6 +5,7 @@ private var endAnimIntro:boolean = false;
 private var endVideo:boolean = false;
 private var alpha:float = 1.0f;
 static var restart:boolean = false; //provisional, millor player prefs
+var posCamMain:GameObject;
 
 private var mainCameraScript:bibliotecaCameraBehaviour;
 //private var mainCameraMovie:MovieTexture;
@@ -15,6 +16,12 @@ var rightDoor:GameObject;
 
 var blackImage:Texture2D;
 var atrezzoOutside:Transform;
+
+var GS_Shop1:GUIStyle;
+var GS_Money:GUIStyle;
+var shop1:Texture;
+var shop2:Texture;
+var shopLvl:int;
 
 function OnGUI() {
 	GUI.color.a = alpha;
@@ -31,27 +38,79 @@ function OnGUI() {
 	    	}
     	}
     	else {
-    		if (GUI.Button(Rect (Screen.width/2,Screen.height/2,Screen.height/8,Screen.height/8), "MENU")) {
-	    		shopping = false;
-	    		SwapInMenu();
+    		if (alpha <= 0.0f)  {
+				alpha = 0.0f;
+	    		if(shopLvl == 0){
+		    		GUI.DrawTexture(Rect (0,0,Screen.width,Screen.height),shop1);
+		    		if (GUI.Button(Rect (Screen.width/2,Screen.height/2,Screen.height/8,Screen.height/8),"menu",GS_Shop1)) {
+			    		shopping = false;
+			    		SwapInMenu();
+			    	}else if (GUI.Button(Rect((2.26*Screen.width)/67.73,(12.6*Screen.height)/38.1,(11.99*Screen.width)/67.73,(16.36*Screen.height)/38.1),"",GS_Shop1)) {
+			    		shopLvl = 1;
+			    		Debug.Log("1");
+			    	}else if (GUI.Button(Rect((15*Screen.width)/67.73,(12.6*Screen.height)/38.1,(11.99*Screen.width)/67.73,(16.36*Screen.height)/38.1),"",GS_Shop1)) {
+			    		Debug.Log("2");
+			    	}else if (GUI.Button(Rect((27.77*Screen.width)/67.73,(12.6*Screen.height)/38.1,(11.99*Screen.width)/67.73,(16.36*Screen.height)/38.1),"",GS_Shop1)) {
+			    		Debug.Log("3");
+			    	}else if (GUI.Button(Rect((40.46*Screen.width)/67.73,(12.6*Screen.height)/38.1,(11.99*Screen.width)/67.73,(16.36*Screen.height)/38.1),"",GS_Shop1)) {
+			    		Debug.Log("4");
+			    	}else if (GUI.Button(Rect((53.24*Screen.width)/67.73,(12.6*Screen.height)/38.1,(11.99*Screen.width)/67.73,(16.36*Screen.height)/38.1),"",GS_Shop1)) {
+			    		Debug.Log("5");
+			    	}
+			    	GS_Money.fontSize = Screen.height/15;
+    				GUI.Label (new Rect (Screen.width/2, Screen.height/15, 1, 1), ""+PlayerPrefs.GetInt("money"), GS_Money);
+		    	}else if(shopLvl == 1){
+		    		GUI.DrawTexture(Rect (0,0,Screen.width,Screen.height),shop2);
+		    		if (GUI.Button(Rect (Screen.width/2,Screen.height/2,Screen.height/8,Screen.height/8),"menu",GS_Shop1)) shopLvl = 0;
+			    	
+			    	/*if (GUI.Button(Rect((2.26*Screen.width)/67.73,(12.6*Screen.height)/38.1,(11.99*Screen.width)/67.73,(16.36*Screen.height)/38.1),"",GS_Shop1)) {
+			    		Debug.Log("LLibreB");
+			    	}if (GUI.Button(Rect((15*Screen.width)/67.73,(12.6*Screen.height)/38.1,(11.99*Screen.width)/67.73,(16.36*Screen.height)/38.1),"",GS_Shop1)) {
+			    		Debug.Log("2");
+			    	}if (GUI.Button(Rect((27.77*Screen.width)/67.73,(12.6*Screen.height)/38.1,(11.99*Screen.width)/67.73,(16.36*Screen.height)/38.1),"",GS_Shop1)) {
+			    		Debug.Log("3");
+			    	}if (GUI.Button(Rect((40.46*Screen.width)/67.73,(12.6*Screen.height)/38.1,(11.99*Screen.width)/67.73,(16.36*Screen.height)/38.1),"",GS_Shop1)) {
+			    		Debug.Log("4");
+			    	}if (GUI.Button(Rect((53.24*Screen.width)/67.73,(12.6*Screen.height)/38.1,(11.99*Screen.width)/67.73,(16.36*Screen.height)/38.1),"",GS_Shop1)) {
+			    		Debug.Log("5");
+			    	}*/
+		    	}
 	    	}
+			else{
+				GUI.color.a = 1.0f;
+				GUI.DrawTexture(Rect (0,0,Screen.width,Screen.height),shop1);
+				GUI.color.a = alpha;
+				GUI.DrawTexture(new Rect(0,0,Screen.width, Screen.height), blackImage);
+				alpha -= Time.deltaTime;
+			}
     	}
 	}
 }
 
 function SwapInMenu() {
+	endAnimIntro = false;
 	if (shopping) {
 		mainCamera.SendMessage("PlayChangeToShop");
     	yield WaitForSeconds(mainCameraScript.GetAnimLength(1));
+    	alpha = 1.0f;
 	} else {
 		mainCamera.SendMessage("PlayChangeToMenu");
     	yield WaitForSeconds(mainCameraScript.GetAnimLength(2));
     }
+    endAnimIntro = true;
 }
 	
 function Awake() {
+	shopLvl = 0;
 	if (!restart) PlayIntro("IntroPageFlip4.mp4");
-	else endVideo = true;
+	else{
+		for (var child:Transform in atrezzoOutside) (child.gameObject).SetActive(false);
+		mainCamera.transform.position = posCamMain.transform.position;
+		mainCamera.transform.rotation = posCamMain.transform.rotation;
+		endVideo = true;
+		endAnimIntro = true;
+	}
+	
 }
 
 function Start() {
@@ -59,6 +118,8 @@ function Start() {
 	//mainCameraMovie = mainCamera.GetComponent(Renderer).material.mainTexture as MovieTexture;
 	leftDoor.GetComponent(Animation).enabled = false;
 	rightDoor.GetComponent(Animation).enabled = false;
+	
+	
 }
 
 function PlayIntro(videoPath:String) {
@@ -67,14 +128,13 @@ function PlayIntro(videoPath:String) {
  	alpha = 1.0f;
  	Debug.Log("Video playback completed.");
  	endVideo = true;
- 	restart = true;
 }
 
 function Update() {
 	if (endVideo) {
 		if (alpha <= 0.0f)  {
 			endVideo = false;
-			EnterMainMenu();
+			if(!restart) EnterMainMenu();
 		}
 		else alpha -= Time.deltaTime*0.4f;
 	}
@@ -94,4 +154,5 @@ function EnterMainMenu() {
 	leftDoor.SetActive(false);
 	rightDoor.SetActive(false);
 	endAnimIntro = true;
+	restart = true;
 }
